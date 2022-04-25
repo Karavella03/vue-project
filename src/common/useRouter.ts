0eb4badbd0d4b813b1bd9dcf10f0a05e3a0eacf7
Karavella03 <nikitke06060@gmail.com>
2022-04-25 17:00:18 +0500

    Implement get search param method

--- src/common/useRouter.ts
+++ src/common/useRouter.ts
@@ -1,4 +1,4 @@
-import { Component, computed, ComputedRef } from 'vue';
+import { Component, computed, ComputedRef, ref } from 'vue';
 import { useStore } from 'vuex';
 import { State } from '@/store/state';
 import { RouterMap, RoutesEnum } from '@/constants/routes';
@@ -6,12 +6,14 @@ import { RouterMap, RoutesEnum } from '@/constants/routes';
 export interface IUseRouter {
   hash: ComputedRef<string>;
   CurrentPage: ComputedRef<Component>;
-  redirect: (route: RoutesEnum) => void;
+  redirect: (route: RoutesEnum, search?: Record<string, string>) => void;
+  getParam: (key: string) => string | null;
 }
 
 export const useRouter = (): IUseRouter => {
   const store = useStore<State>();
   const hash = computed(() => store.state.locationHash.toLowerCase());
+  const params = ref(new URLSearchParams(location.search));
   const CurrentPage = computed(() => {
     if (hash.value in RouterMap) {
       return RouterMap[hash.value as RoutesEnum];
@@ -19,12 +21,21 @@ export const useRouter = (): IUseRouter => {
     return RouterMap[RoutesEnum.MAIN];
   });
 
-  const redirect = (route: RoutesEnum) => {
+  const redirect = (route: RoutesEnum, search?: Record<string, string>) => {
+    const searchParams = new URLSearchParams(search).toString();
+    history.replaceState(null, '', `?${searchParams}`);
     window.location.hash = `#${route}`;
+    params.value = new URLSearchParams(location.search);
   };
+
+  const getParam = (key: string) => {
+    return params.value.get(key);
+  };
+
   return {
     hash,
     CurrentPage,
     redirect,
+    getParam,
   };
 };
--- src/modules/Blog/BlogWrapper.vue
+++ src/modules/Blog/BlogWrapper.vue
@@ -1,9 +1,21 @@
 <script setup lang="ts">
 import ViewContainer from '@/components/ViewContainer';
+import { useRouter } from '@/common/useRouter';
+import { RoutesEnum } from '@/constants/routes';
+
+const router = useRouter();
 </script>
 
 <script lang="ts">
 export default {};
 </script>
 
-<template><ViewContainer>Blog</ViewContainer></template>
+<template>
+  <ViewContainer
+    >Blog
+    <button @click="router.redirect(RoutesEnum.BLOG, { param: 'param' })">
+      redirect check
+    </button>
+    {{ router.getParam('param') }}
+  </ViewContainer>
+</template>
